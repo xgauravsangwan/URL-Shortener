c36d9de9468998cf160024c4fb71318a057f71f7
xgauravsangwan <164604008+xgauravsangwan@users.noreply.github.com>
2025-10-08 10:13:40 +0530

    Add files via upload

---
+++ url-shortener-sb/src/main/java/com/url/shortener/dtos/ClickEventDTO.java
@@ -0,0 +1,13 @@
+package com.url.shortener.dtos;
+
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+public class ClickEventDTO {
+    private LocalDate clickDate;
+    private Long count;
+}
---
+++ url-shortener-sb/src/main/java/com/url/shortener/dtos/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.url.shortener.dtos;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class RegisterRequest {
+
+    private String username;
+    private String email;
+    private Set<String> roles;
+    private String password;
+}
